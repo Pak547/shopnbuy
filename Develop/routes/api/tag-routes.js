3b3c1709f52fd02866623ddbee81f2594ac874ae
Pak <pakapongm@gmail.com>
2023-12-23 03:52:02 -0800

    added on tags findall method

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -4,6 +4,19 @@ const { Tag, Product, ProductTag } = require('../../models');
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
+  Tag.findAll({
+    include: [
+      {
+        model: Product,
+        attributes: ['tag_name'],
+      },
+    ],
+  })
+    .then((tagData) => res.json(tagData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
   // find all tags
   // be sure to include its associated Product data
 });
